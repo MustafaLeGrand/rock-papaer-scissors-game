15709104c50382cd2577dfdb40a663a3be689765
Raul Garcia Lagunas <rgcoloman@gmail.com>
2022-07-22 13:58:46 -0500

    change icons, correct switch-case, add defineWinner function, add new scoreboard in html

--- main.js
+++ main.js
@@ -1,54 +1,83 @@
 // Computer random selection 
-function computerPlay() {
+let computerPlay = () => {
     //Game options to play with 
     let options = ['Rock', 'Paper', 'Scissors'];
     //Random number generator
-    let randomNumber = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * options.length);
     //Select random option from array
     let computerSelection = options[randomNumber];
     // return value so the game can be played
     return computerSelection;
 }
 
-let computerSelection = computerPlay();
-
-let playerSelection = computerPlay();
-
 //Round
-let round = 1;
+let round = 0;
 
 //Add round
-function addRound () {
-    round++;
-}
+let addRound = () => round += 1;
 
 //Scores
 let playerScore = 0;
 let computerScore = 0;
 
 // Start round, define winner, loser or tie game 
-function playRound (playerSelection, computerSelection) {
-    
+function playRound () {
+// Selection of user & comp
+    let computerSelection = computerPlay();
+    let playerSelection = computerPlay();
+    addRound();
+
     //concatenate strings to match cases 
     switch (playerSelection + computerSelection) {
         // Tie case
         case 'RockRock':
         case 'PaperPaper':
         case 'ScissorsScissors':
-            return 'Tie Game!'; 
+            computerScore += 0;
+            playerScore += 0;
+            return ('Tie Game!');
         // Player score
         case 'RockScissors':
         case 'PaperRock':
         case 'ScissorsPaper':
-            return `${playerSelection} beats ${computerSelection}, You Won!`, playerScore += 1;
+            playerScore += 1;
+            return (`${playerSelection} beats ${computerSelection}, You Won!`);
         // Computer score    
         case 'ScissorsRock':
         case 'RockPaper':
         case 'PaperScissors':
-            return `${computerSelection} beats ${playerSelection}, Computer Won :(`, computerScore += 1;
+            computerScore += 1;
+            return (`${computerSelection} beats ${playerSelection}, Computer Won :(`);
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+defineWinner = () => {
+    if (playerScore > computerScore) {
+        return 'Cheers, You won!';
+    } else if (playerScore == computerScore) {
+        return "It's a tie!";
+    } else if (computerScore > playerScore) {
+        return 'Computer won, try again!';
+    }
+}
 
+endGame = () => {
+    
+}
 
+console.log(playRound());
+console.log(playerScore, computerScore, round);
+
+console.log(playRound());
+console.log(playerScore, computerScore, round);
+
+console.log(playRound());
+console.log(playerScore, computerScore, round);
+
+console.log(playRound());
+console.log(playerScore, computerScore, round);
+
+console.log(playRound());
+console.log(playerScore, computerScore, round);
+
+console.log(defineWinner())
