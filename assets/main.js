453c79b74379e2d9578c30f7747616473cd3eb4a
Raul Garcia Lagunas <rgcoloman@gmail.com>
2023-04-26 17:48:53 -0600

    solve error in logic

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,8 @@
-// Computer random selection 
-let computerPlay = () => {
-    //Game options to play with 
-    let options = ['Rock', 'Paper', 'Scissors'];
+// Computer random selection
+let computerPlay = () => 
+{
+    //Comp options
+    let options = ['rock', 'paper', 'scissors'];
     //Random number generator
     let randomNumber = Math.floor(Math.random() * options.length);
     //Select random option from array
@@ -10,87 +11,107 @@ let computerPlay = () => {
     return computerSelection;
 }
 
-//Round
-let round = 0;
+// Displays
+// Round display
+let roundDisplay = document.getElementById('round');
+// Score displays
+let pScoreDisplay = document.getElementById('playerScore');
+let cScoreDisplay = document.getElementById('computerScore');
+// Results display
+let resultDisplay = document.getElementById('results');
 
+// Buttons
+// player buttons
+let buttons = document.querySelectorAll('button');
+
+// Round
+let round = 1;
+roundDisplay.textContent = round;
 //Add round
 let addRound = () => round += 1;
 
-//Scores
+// Scores
 let playerScore = 0;
 let computerScore = 0;
+pScoreDisplay.textContent = playerScore;
+cScoreDisplay.textContent = computerScore;
+
+// updateRound helper
+let updateRound = () => 
+{
+        roundDisplay.textContent = round
+}
+
+// updateScore helper 
+let updateScore = () => 
+{
+    pScoreDisplay.textContent = playerScore;
+    cScoreDisplay.textContent = computerScore;
+}
+
+// Define tie game, winner or loser
+let defineWinner = () => 
+{
+    if (playerScore > computerScore && round >= 5) {
+        textDisplay.innerHTML = 'Cheers, You won!';
+    } else if (playerScore == computerScore && round >= 5) {
+        textDisplay.innerHTML = 'Game!';
+    } else if (computerScore > playerScore && round >= 5) {
+        textDisplay.innerHTML = 'Computer won 0_0, try again!';
+    } else if (playerScore == 5) {
+        textDisplay.innerHTML = 'Flawless Victory!'
+    }
+}
+
+// Finish the game
+let endgame = () => 
+{
+    if (round == 5) {
+        round = 1;
+        pScoreDisplay.innerHTML = 0;
+        cScoreDisplay.innerHTML = 0;
+    }
+}
 
 // Add round, sum points to round winner
-function playRound () {
-// Selection of user & comp
+function playRound (e) {
+    e.stopPropagation();
+
+    // set playerSelection to 
+    let playerSelection = this.id;
+    console.log(`player selected: ${playerSelection}`);
+    // set computerSelection to
     let computerSelection = computerPlay();
-    let playerSelection = computerPlay();
-// Add up the rounds    
-    addRound();
-    //concatenate strings to match cases 
-    switch (playerSelection + computerSelection) {
+    console.log(`computer selected: ${computerSelection}`);
+
+    switch (playerSelection + computerSelection) 
+    {
         // Tie case
-        case 'RockRock':
-        case 'PaperPaper':
-        case 'ScissorsScissors':
-            computerScore += 0;
-            playerScore += 0;
-            return ('Tie!');
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            resultDisplay.textContent = 'Tie!';
+            break;
         // Player score
-        case 'RockScissors':
-        case 'PaperRock':
-        case 'ScissorsPaper':
-            playerScore += 1;
-            return (`${playerSelection} beats ${computerSelection}, You get the round!`);
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            playerScore ++;
+            resultDisplay.textContent = `${playerSelection} beats ${computerSelection}, You get the round!`;
+            break;
         // Computer score    
-        case 'ScissorsRock':
-        case 'RockPaper':
-        case 'PaperScissors':
-            computerScore += 1;
-            return (`${computerSelection} beats ${playerSelection}, round for computer :(`);
+        case 'scissorsrock':
+        case 'rockpaper':
+        case 'paperscissors':
+            computerScore ++;
+            resultDisplay.textContent = `${computerSelection} beats ${playerSelection}, round for computer :(`;
+            break;
     }
+    addRound();
+    updateRound();
+    updateScore();
 }
 
-//Define tie game, winner or loser of the match 
-defineWinner = () => {
-    if (playerScore > computerScore) {
-        return 'Cheers, You won!';
-    } else if (playerScore == computerScore) {
-        return "Game!";
-    } else if (computerScore > playerScore) {
-        return 'Computer won the match 0_0, try again!';
-    } else if (playerScore == 5) {
-        return 'Flawless Victory!'
-    }
-}
-
-endgame = () => {
-    if (round == 5) {
-        
-    }
-}
-
-// console.log(playRound());
-// console.log(playerScore, computerScore, round);
-
-// console.log(playRound());
-// console.log(playerScore, computerScore, round);
-
-// console.log(playRound());
-// console.log(playerScore, computerScore, round);
-
-// console.log(playRound());
-// console.log(playerScore, computerScore, round);
-
-// console.log(playRound());
-// console.log(playerScore, computerScore, round);
-
-// console.log(defineWinner())
-
-let buttons = document.querySelectorAll('button');
-
-console.log(buttons);
-
-
-
-
+buttons.forEach(button => button.addEventListener('click', playRound, {
+    // once: true,
+}));
